ec14ef2e19043eb3773c9433abd2df00cc1cd446
scatic1 <scatic1@etf.unsa.ba>
2018-04-03 19:32:18 +0200

    Eureka

--- IdentifyMicroservice/src/main/java/com/sweetcart/demo/IdentifyApplication.java
+++ IdentifyMicroservice/src/main/java/com/sweetcart/demo/IdentifyApplication.java
@@ -2,8 +2,13 @@ package com.sweetcart.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+
+@EnableEurekaClient
 public class IdentifyApplication {
 
 	public static void main(String[] args) {
